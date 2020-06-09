332ba588c75137411783544d74213c12aea259f5
Tilottoma Bhattacharyya <62555308+Tilottoma1707@users.noreply.github.com>
2020-06-09 12:29:31 +0530

    fight to live

---
+++ asteroid.js
@@ -0,0 +1,60 @@
+function asteroid(pos, r) {
+    if (pos) {
+      this.pos = pos.copy();
+    } else {
+      this.pos = createVector(random(width), random(height));
+    }
+    if (r) {
+      this.r = r * 0.5;
+    } else {
+      this.r = random(15, 50);
+    }
+  
+    this.vel = p5.Vector.random2D();
+    this.total = floor(random(5, 15));
+    this.offset = [];
+    for (var i = 0; i < this.total; i++) {
+      this.offset[i] = random(-this.r * 0.5, this.r * 0.5);
+    }
+  
+this.update = function(){
+    this.pos.add(this.vel)
+}
+
+    this.render = function(){
+        push();
+        noFill();
+        stroke(255);
+        translate(this.pos.x,this.pos.y);
+        beginShape();
+        for(var i = 0;i < this.total; i++){
+            var angle = map(i,0,this.total,0,TWO_PI);
+            var r =  this.r + this.offset[i]
+            var x = r*cos(angle);
+            var y =  r*sin(angle);
+            vertex(x,y);
+        }
+        endShape(CLOSE);
+      //  ellipse(0,0,this.r*2);
+        pop();
+    }
+    this.break = function(){
+    var newA = [];
+    newA[0] = new asteroid(this.pos,this.r);
+    newA[1] = new asteroid(this.pos,this.r);
+
+    return newA;
+    }
+    this.edges = function() {
+        if (this.pos.x > width + this.r) {
+          this.pos.x = -this.r;
+        } else if (this.pos.x < -this.r) {
+          this.pos.x = width + this.r;
+        }
+        if (this.pos.y > height + this.r) {
+          this.pos.y = -this.r;
+        } else if (this.pos.y < -this.r) {
+          this.pos.y = height + this.r;
+        }
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,77 @@
+var ship;
+
+var asteroids =[];
+
+var lasers = [];
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  ship = new Ship();
+  for(var i = 0; i <10;i++){
+  asteroids.push(new asteroid())
+  }
+}
+
+function draw() {
+  background(0);
+  ship.render();
+  ship.turn();
+  ship.update();
+  //asteroids.render();
+  for(var i = 0; i < asteroids.length;i++){
+    asteroids[i].render();
+    asteroids[i].update();
+    asteroids[i].edges();
+  }
+
+  for (var i = lasers.length - 1; i >= 0; i--) {
+    lasers[i].render();
+    lasers[i].update();
+    if(lasers[i].offscreen()){
+      lasers.splice(i,1);
+    break;
+    }
+          for (var j = asteroids.length - 1; j >= 0; j--) {
+        if (lasers[i].hits(asteroids[j])) {
+          if(asteroids[j].r > 10){
+          if (asteroids[j].r > 10) {
+            var newAsteroids = asteroids[j].break();
+            asteroids = asteroids.concat(newAsteroids);
+          }
+        }
+          asteroids.splice(j, 1);
+          lasers.splice(i, 1);
+          break;
+        }
+      }
+
+      
+  }
+
+ // if(asteroids[i].isTouching(ship.heading)){
+   // rect(0,0,windowWidth,windowHeight)
+  //}
+}
+function keyReleased(){
+  ship.setRotation(0);
+  ship.boosting(false);
+  ship.edges();
+
+  
+}
+function keyPressed(){
+  if(keyCode == 32){
+    lasers.push(new laser(ship.pos,ship.heading));
+  }
+  if(keyCode == RIGHT_ARROW){
+    ship.setRotation(0.1);
+  }else if(keyCode == LEFT_ARROW){
+    ship.setRotation(-0.1);
+  }else if(keyCode == UP_ARROW){
+    ship.boosting(true);
+  }
+}
+
+//function mousePressed(){
+  
+//}
